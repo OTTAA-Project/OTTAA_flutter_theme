4277dff69f014282c0e5b3d077e37c47ebd16429
Emir <emirhassan.ali02@gmail.com>
2022-12-15 02:51:06 -0300

    feat: fixed bugs in appbar

--- example/lib/screens/home.dart
+++ example/lib/screens/home.dart
@@ -4,46 +4,73 @@ import 'package:example/screens/cards_screen.dart';
 import 'package:example/screens/inputs_screen.dart';
 import 'package:example/screens/theme_screen.dart';
 import 'package:flutter/material.dart';
-
+import 'package:ottaa_ui_kit/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
-
   const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   late final TabController _tabController = TabController(length: 5, vsync: this);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('OTTAA Overview Theme'),
-        bottom: TabBar(
+    return SafeArea(
+      child: Scaffold(
+        appBar: OTTAAAppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {},
+            splashRadius: 24,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: Text("Next"),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                splashFactory: NoSplash.splashFactory,
+                enableFeedback: false,
+                foregroundColor: Colors.grey,
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("Next"),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                splashFactory: NoSplash.splashFactory,
+                enableFeedback: false,
+                foregroundColor: Colors.grey,
+              ),
+            ),
+          ],
+          bottom: TabBar(
+            controller: _tabController,
+            isScrollable: true,
+            tabs: const [
+              Tab(text: 'Theme'),
+              Tab(text: 'Buttons'),
+              Tab(text: 'Cards'),
+              Tab(text: 'Inputs'),
+              Tab(text: 'Modals'),
+            ],
+          ),
+        ),
+        body: TabBarView(
           controller: _tabController,
-          isScrollable: true,
-          tabs: const [
-            Tab(text: 'Theme'),
-            Tab(text: 'Buttons'),
-            Tab(text: 'Cards'),
-            Tab(text: 'Inputs'),
-            Tab(text: 'Modals'),
+          children: const [
+            ThemeScreen(),
+            ButtonsScreen(),
+            CardsScreen(),
+            InputsScreen(),
+            ModalsScreen(),
           ],
         ),
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: const [
-          ThemeScreen(),
-          ButtonsScreen(),
-          CardsScreen(),
-          InputsScreen(),
-          ModalsScreen(),
-        ],
-      ),
-   );
+    );
   }
 }
--- lib/src/widgets/appbar/appbar.dart
+++ lib/src/widgets/appbar/appbar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class OTTAAAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const OTTAAAppBar({super.key, this.title, this.leading, this.actions});
+  const OTTAAAppBar({super.key, this.title, this.leading, this.actions, this.bottom, this.center});
 
   final Widget? title;
 
@@ -9,35 +9,70 @@ class OTTAAAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   final List<Widget>? actions;
 
+  final PreferredSizeWidget? bottom;
+
+  final bool? center;
+
   @override
   Widget build(BuildContext context) {
     final canPop = Navigator.of(context).canPop();
 
-    return Container(
-      margin: const EdgeInsets.only(top: 46),
-      padding: const EdgeInsets.only(left: 24, right: 24),
-      child: Flex(
-        direction: Axis.horizontal,
-        children: [
-          buildLeading(canPop, onPressed: () => Navigator.of(context).pop()),
-          if (title != null) title!,
-          if (actions != null) ...actions!,
-        ],
-      ),
+    final leading = buildLeading(canPop, onPressed: () => Navigator.of(context).pop());
+
+    return MediaQuery.removePadding(
+      context: context,
+      removeTop: true,
+      removeBottom: true,
+      removeLeft: true,
+      removeRight: true,
+      child: Container(
+          width: preferredSize.width,
+          height: preferredSize.height,
+          margin: const EdgeInsets.only(top: 46),
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Flex(
+            direction: Axis.vertical,
+            children: [
+              Expanded(
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: [
+                    if (leading != null) leading,
+                    if (title != null)
+                      Flexible(
+                        fit: FlexFit.loose,
+                        child: title!,
+                      ),
+                    if (actions != null)
+                      Flexible(
+                        fit: FlexFit.loose,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: actions!,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              if (bottom != null) bottom!,
+            ],
+          )),
     );
   }
 
-  Widget buildLeading(bool canPop, {required VoidCallback onPressed}) {
+  Widget? buildLeading(bool canPop, {required VoidCallback onPressed}) {
     if (canPop && leading == null) {
       return IconButton(
         icon: const Icon(Icons.arrow_back_ios),
         onPressed: onPressed,
+        splashRadius: 24,
       );
     }
 
-    return leading ?? const SizedBox();
+    return leading;
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(46 + kToolbarHeight);
+  Size get preferredSize => Size.fromHeight(46 + kToolbarHeight + (bottom?.preferredSize.height ?? 0));
 }
